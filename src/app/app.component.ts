e44e44df728442615f999ad8fea6ca252b57e407
AR1988 <37342319+AR1988@users.noreply.github.com>
2024-11-22 19:40:16 +0100

    Add contact creation form
    Refactor contact list to include a contact creation form component. This update also modifies the contact model to use 'firstName' instead of 'name' and refines the contact list display accordingly.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import {Component} from '@angular/core';
 import {ContactListComponent} from './components/contact-list/contact-list.component';
-import {
-  ContactListElementComponent
-} from './components/contact-list/contact-list-element/contact-list-element.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ContactListComponent, ContactListElementComponent],
+  imports: [ContactListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
